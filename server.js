46e89c9565f80d27e3db6691cdceb954f173a18d
mfmunic <mfmunic@gmail.com>
2017-07-15 12:47:35 -0700

    added server and restaurant.js

---
+++ server.js
@@ -0,0 +1,68 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Star Wars Characters (DATA)
+// =============================================================
+var tables = [{
+  table: 1,
+  occ: false
+}, {
+  table: 2,
+  occ: false
+}, {
+  table: 3,
+  occ: false
+}, {
+  table: 4,
+  occ: false
+}, {
+  table: 5,
+  occ: false
+}];
+
+var waitlist = []
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/home", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/make", function(req, res) {
+  res.sendFile(path.join(__dirname, "make.html"));
+});
+
+// Search for Specific Character (or all characters) - provides JSON
+app.get("/api/tables", function(req, res) {
+  res.json(tables);
+});
+
+app.get("/api/waitlist", function(req, res) {
+  res.json(waitlist);
+});
+
+Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
